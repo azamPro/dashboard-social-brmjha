537c15fdb1c06046c17bec3eebb6fc1827a3faac
Jawahirah <jalsefri0004@stu.kau.edu.sa>
2023-07-22 05:42:38 +0300

    feat: Create a route to handle GET request, update data on an HTML page with user data, and check user credentials
    This commit adds a new route to the application that handles GET requests. The route is responsible for retrieving user data from the server and updating the HTML page with the fetched information.
    
    Additionally, this commit implements a user credential check mechanism to ensure that only authenticated users can access the requested data. The credentials are validated against the stored user credentials in the system.

--- server.js
+++ server.js
@@ -15,6 +15,21 @@ webApp.use(cors());
 webApp.use(express.static('website'));
 // Local server
 const port = 8080;
-webApp.listen(port,()=>{
+webApp.listen(port, () => {
     console.log('200');
 });
+// Dummy API endpoint 
+let users = {
+    user1: { username: 'Jawahirah', password: '1234',email:'Jawahirah@gmail.com', like: 65, comment: 3, tweet: 33, views: 300 },
+    user2: { username: 'azamPro', password: '1234',email:'azamPro@gmail.com', like: 50, comment: 10, tweet: 45, views: 200 },
+    user3: { username: 'nuhaS', password: '1234',email:'nuhaS@gmail.com' ,like: 20, comment: 15, tweet: 20, views: 4000 },
+    user4: { username: 'hanadi16', password: '1234ww',email:'hanoali@gmail.com', like: 100, comment: 11, tweet: 45, views: 1000 },
+
+}
+// Send users data to the client-side 
+webApp.get('/getData',(req,res)=>{
+   res.send(users)
+})
+
+
+
